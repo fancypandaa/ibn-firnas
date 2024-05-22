6d1f9d31abddb6d94718b30a33dd17ed25bbdb90
liosub <mlio64565@gmail.com>
2024-05-22 21:58:21 +0100

    default spring booot main

--- src/main/java/com/flights/book/BookApplication.java
+++ src/main/java/com/flights/book/BookApplication.java
@@ -1,9 +1,13 @@
 package com.flights.book;
 
+import com.flights.book.config.RsaKeyConfigProperties;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(RsaKeyConfigProperties.class)
 public class BookApplication {
 
 	public static void main(String[] args) {
