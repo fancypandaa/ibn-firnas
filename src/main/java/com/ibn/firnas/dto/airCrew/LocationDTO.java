b02cf0c95346a6035de57916c4825e2fee0b87bd
liosub <mlio64565@gmail.com>
2024-08-21 19:40:20 +0100

    set validation constrains to location dto

--- src/main/java/com/ibn/firnas/dto/airCrew/LocationDTO.java
+++ src/main/java/com/ibn/firnas/dto/airCrew/LocationDTO.java
@@ -1,14 +1,29 @@
 package com.ibn.firnas.dto.airCrew;
 
 
-public record LocationDTO(Long locationId,String ipAddress,String country,String time, String lat, String lng,Long userId) {
-    public LocationDTO(Long locationId, String ipAddress, String country, String time, String lat, String lng,Long userId) {
-        this.locationId = locationId;
-        this.ipAddress = ipAddress;
-        this.country = country;
-        this.time = time;
-        this.lat = lat;
-        this.lng = lng;
-        this.userId=userId;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+
+public record LocationDTO(Long locationId,
+                          @NotBlank(message = "Make sure to Set IP Address")
+                          @Pattern(regexp="^[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}$")
+                          String ipAddress,
+                          @NotBlank(message = "country cannot be null")
+                          String country,
+                          @NotBlank(message = "time cannot be null")
+                          String time,
+
+                          @NotBlank(message = "lat cannot be null")
+                          String lat,
+
+                          @NotBlank(message = "lng cannot be null")
+                          String lng,
+
+                          @NotBlank(message = "isActive cannot be null")
+                          Boolean isActive
+) {
+
+    public LocationDTO(String ipAddress, String country, String time, String lat, String lng, Boolean isActive) {
+        this(null, ipAddress, country, time, lat, lng, isActive);
     }
 }
