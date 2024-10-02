ac505701c151985f86d3e2539cde054dee4016f9
liosub <mlio64565@gmail.com>
2024-10-02 23:38:21 +0100

    remove throws exceptions

--- src/main/java/com/ibn/firnas/service/FlightService.java
+++ src/main/java/com/ibn/firnas/service/FlightService.java
@@ -1,10 +1,9 @@
 package com.ibn.firnas.service;
 
 import com.ibn.firnas.dto.airCrew.FlightDTO;
-import com.ibn.firnas.exception.CustomException;
 
 public interface FlightService {
-    FlightDTO findFlightsById(Long flightId) throws CustomException;
-    FlightDTO createNewFlight(FlightDTO flightDTO) throws CustomException;
-    FlightDTO updateExistsFlight(Long flightId, FlightDTO flightDTO) throws CustomException;
+    FlightDTO findFlightsById(Long flightId);
+    FlightDTO createNewFlight(FlightDTO flightDTO);
+    FlightDTO updateExistsFlight(Long flightId, FlightDTO flightDTO);
 }
