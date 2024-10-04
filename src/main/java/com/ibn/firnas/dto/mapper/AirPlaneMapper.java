ea53cc32896726c5a779afa52cb4d8e7fb1d72d0
liosub <mlio64565@gmail.com>
2024-10-04 01:13:15 +0100

    add update method in mapper

--- src/main/java/com/ibn/firnas/dto/mapper/AirPlaneMapper.java
+++ src/main/java/com/ibn/firnas/dto/mapper/AirPlaneMapper.java
@@ -2,7 +2,10 @@ package com.ibn.firnas.dto.mapper;
 
 import com.ibn.firnas.domain.AirPlane;
 import com.ibn.firnas.dto.airCrew.AirPlaneDTO;
+import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
 import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring")
@@ -10,4 +13,6 @@ public interface AirPlaneMapper {
     AirPlaneMapper INSTANCE= Mappers.getMapper(AirPlaneMapper.class);
     AirPlane airPlaneDTOtoAirPlane(AirPlaneDTO airPlaneDTO);
     AirPlaneDTO airPlaneToAirPlaneDTO(AirPlane airPlane);
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void updateAirPlaneFromDTO(AirPlaneDTO airPlaneDTO, @MappingTarget AirPlane airPlane);
 }
