dce815f0b914451c91c1a9a8e6bfcab3b17fc270
liosub <mlio64565@gmail.com>
2024-10-04 01:23:49 +0100

    change exception from try-catch to global Exception advice

--- src/main/java/com/ibn/firnas/controller/LocationController.java
+++ src/main/java/com/ibn/firnas/controller/LocationController.java
@@ -1,7 +1,6 @@
 package com.ibn.firnas.controller;
 
 import com.ibn.firnas.dto.airCrew.LocationDTO;
-import com.ibn.firnas.exception.CustomException;
 import com.ibn.firnas.service.LocationService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,44 +16,23 @@ public class LocationController {
     }
     @GetMapping("/{locationId}")
     public ResponseEntity<? super LocationDTO> findLocationById(@PathVariable Long locationId){
-        try {
-            LocationDTO locationDTO=locationService.findLocationById(locationId);
-            return new ResponseEntity<>(locationDTO, HttpStatus.OK);
-        }
-        catch (CustomException exception){
-            return new ResponseEntity<>(exception.getMessage(),HttpStatus.BAD_REQUEST);
-        }
+        LocationDTO locationDTO=locationService.findLocationById(locationId);
+        return new ResponseEntity<>(locationDTO, HttpStatus.OK);
     }
     @GetMapping("/byUser/{userId}")
     public ResponseEntity<? super LocationDTO> findLocationsByUserId(@PathVariable Long userId){
-        try {
-            List<LocationDTO> locationDTOList=locationService.findAllLocationsByUserId(userId);
-            return new ResponseEntity<>(locationDTOList, HttpStatus.OK);
-        }
-        catch (CustomException exception){
-            return new ResponseEntity<>(exception.getMessage(),HttpStatus.BAD_REQUEST);
-        }
+        List<LocationDTO> locationDTOList=locationService.findAllLocationsByUserId(userId);
+        return new ResponseEntity<>(locationDTOList, HttpStatus.OK);
     }
-    @PostMapping
-    public ResponseEntity<? super LocationDTO> createNewLocation(@RequestBody LocationDTO locationDTO){
-        try {
-            LocationDTO savedLocationDTO=locationService.createNewLocationForUser(locationDTO);
-            return new ResponseEntity<>(savedLocationDTO, HttpStatus.OK);
-        }
-        catch (CustomException exception){
-            return new ResponseEntity<>(exception.getMessage(),HttpStatus.BAD_REQUEST);
-        }
+    @PostMapping("/{userId}")
+    public ResponseEntity<? super LocationDTO> createNewLocation(@PathVariable Long userId,@RequestBody LocationDTO locationDTO){
+        LocationDTO savedLocationDTO=locationService.createNewLocationForUser(userId,locationDTO);
+        return new ResponseEntity<>(savedLocationDTO, HttpStatus.OK);
     }
 
     @PutMapping("/{locationId}")
     public ResponseEntity<? super LocationDTO> updateExistLocation(@PathVariable Long locationId,@RequestBody LocationDTO locationDTO){
-        try {
-            LocationDTO savedLocationDTO=locationService.updateLocationDetails(locationId,locationDTO);
-            return new ResponseEntity<>(locationDTO, HttpStatus.OK);
-        }
-        catch (CustomException exception){
-            return new ResponseEntity<>(exception.getMessage(),HttpStatus.BAD_REQUEST);
-        }
+        LocationDTO savedLocationDTO=locationService.updateLocationDetails(locationId,locationDTO);
+        return new ResponseEntity<>(savedLocationDTO, HttpStatus.OK);
     }
-
 }
