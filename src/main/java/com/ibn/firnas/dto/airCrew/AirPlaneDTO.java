787612c2ffb501411f69d127731f1ece4c65d53b
liosub <mlio64565@gmail.com>
2024-10-04 01:14:41 +0100

    add validations to airplaneDto fields

--- src/main/java/com/ibn/firnas/dto/airCrew/AirPlaneDTO.java
+++ src/main/java/com/ibn/firnas/dto/airCrew/AirPlaneDTO.java
@@ -1,9 +1,31 @@
 package com.ibn.firnas.dto.airCrew;
 
 
+
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+
 import java.util.Date;
 
-public record AirPlaneDTO( Long planeId,
-         String companyName, String planeName,
-         Integer airline, String airlineLogo,  Date createdAt, Date updateAt) {
+public record AirPlaneDTO(
+        Long planeId,
+        @NotBlank(message = "companyName cannot be null")
+        String companyName,
+        @NotBlank(message = "planeName cannot be null")
+        String planeName,
+        @NotBlank(message = "planeName cannot be null")
+        String airline,
+        String model,
+        String airlineLogo,
+        @Max(100000)
+        Integer totalFlightHours,
+        Integer fuelTankCapacity,
+        Date lastCheckIn,
+        @Min(3)
+        Integer numberOfSeats,
+        Date createdAt,
+        Date updateAt) {
 }
+
+
