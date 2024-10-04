4bc8ada7ff3824614a65e2eac1c58788e48d4cd4
liosub <mlio64565@gmail.com>
2024-10-04 01:24:10 +0100

    remove unused throws custom Exception

--- src/main/java/com/ibn/firnas/service/impl/LocationServiceImpl.java
+++ src/main/java/com/ibn/firnas/service/impl/LocationServiceImpl.java
@@ -1,20 +1,20 @@
-package com.ibn.firnas.service;
+package com.ibn.firnas.service.impl;
 
 import com.ibn.firnas.domain.Location;
 import com.ibn.firnas.domain.UserDetails;
 import com.ibn.firnas.dto.airCrew.LocationDTO;
 import com.ibn.firnas.dto.mapper.LocationMapper;
-import com.ibn.firnas.exception.CustomException;
+import com.ibn.firnas.exception.CustomNotFoundException;
 import com.ibn.firnas.repostiories.LocationRepository;
 import com.ibn.firnas.repostiories.UserDetailsRepository;
+import com.ibn.firnas.service.LocationService;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import java.util.List;
-import java.util.Objects;
 import java.util.stream.*;
 import java.util.Optional;
 @Service
-public class LocationServiceImpl implements LocationService{
+public class LocationServiceImpl implements LocationService {
     private final LocationRepository locationRepository;
     private final LocationMapper locationMapper;
     private final UserDetailsRepository userDetailsRepository;
@@ -25,30 +25,30 @@ public class LocationServiceImpl implements LocationService{
     }
 
     @Override
-    public LocationDTO findLocationById(Long locationId) throws CustomException{
+    public LocationDTO findLocationById(Long locationId) {
         Optional<Location> optionalLocation = locationRepository.findById(locationId);
         if(!optionalLocation.isPresent()){
-            throw new CustomException("Location Not Found");
+            throw new CustomNotFoundException("Location Not Found");
         }
         return locationMapper.locationToLocationDTO(optionalLocation.get());
     }
     @Override
-    public List<LocationDTO> findAllLocationsByUserId(Long userId) throws CustomException {
+    public List<LocationDTO> findAllLocationsByUserId(Long userId) {
         List<LocationDTO> locationDTOS=locationRepository
                 .findLocationsByUserDetails_UserIdOrderByCreatedAtDesc(userId)
                 .stream()
                 .map(locationMapper::locationToLocationDTO)
                 .collect(Collectors.toList());
         if(locationDTOS.size() < 1){
-            throw new CustomException("Location Not Found for user "+userId);
+            throw new CustomNotFoundException("Location Not Found for user "+userId);
         }
         return locationDTOS;
     }
     @Override
-    public LocationDTO createNewLocationForUser(Long userId,LocationDTO locationDTO) throws CustomException {
+    public LocationDTO createNewLocationForUser(Long userId,LocationDTO locationDTO) {
         Optional<UserDetails> userDetails = userDetailsRepository.findById(userId);
         if(!userDetails.isPresent()){
-            throw new CustomException("User Not found");
+            throw new CustomNotFoundException("User Not found");
         }
         Location newLocation = locationMapper.locationDTOtoLocation(locationDTO);
         newLocation.setUserDetails(userDetails.get());
@@ -56,9 +56,9 @@ public class LocationServiceImpl implements LocationService{
         return locationMapper.locationToLocationDTO(location);
     }
     @Override
-    public LocationDTO updateLocationDetails(Long locationId, LocationDTO locationDTO) throws CustomException {
+    public LocationDTO updateLocationDetails(Long locationId, LocationDTO locationDTO) {
         if(!locationRepository.existsById(locationId)){
-            throw new CustomException("Custom Not Found", HttpStatus.NOT_FOUND);
+            throw new CustomNotFoundException("Custom Not Found");
         }
         Location location = locationMapper.locationDTOtoLocation(locationDTO);
         location.setLocationId(locationId);
