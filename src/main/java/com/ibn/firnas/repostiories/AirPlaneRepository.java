44375fac5b1822c12b6862e295ffca28c2260d82
liosub <mlio64565@gmail.com>
2024-10-09 20:23:55 +0100

    remove space

--- src/main/java/com/ibn/firnas/repostiories/AirPlaneRepository.java
+++ src/main/java/com/ibn/firnas/repostiories/AirPlaneRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AirPlaneRepository extends JpaRepository< AirPlane,Long> {
+public interface AirPlaneRepository extends JpaRepository<AirPlane,Long> {
 }
