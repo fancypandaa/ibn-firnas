995748070b1913b451be0563d3526a0e8c7d5cb7
liosub <mlio64565@gmail.com>
2024-10-09 20:21:33 +0100

    add methods for assign and manage flight crew

--- src/main/java/com/ibn/firnas/controller/FlightController.java
+++ src/main/java/com/ibn/firnas/controller/FlightController.java
@@ -1,15 +1,15 @@
 package com.ibn.firnas.controller;
 
+import com.ibn.firnas.dto.airCrew.FlightAllDTO;
 import com.ibn.firnas.dto.airCrew.FlightDTO;
-import com.ibn.firnas.exception.CustomNotFoundException;
 import com.ibn.firnas.service.FlightService;
-import com.ibn.firnas.utils.ErrorMapper;
 import jakarta.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import java.util.*;
 @RestController
 @RequestMapping(FlightController.FLIGHT_URI)
 public class FlightController {
@@ -19,6 +19,11 @@ public class FlightController {
 
     public FlightController(FlightService flightService) {
         this.flightService = flightService;
+    }
+    @GetMapping("/flightCrew/{flightId}")
+    public ResponseEntity<? super FlightAllDTO> getFlightAndCrewById(@PathVariable Long flightId){
+        FlightAllDTO flightDTO= flightService.findFlightAndCrewById(flightId);
+        return new ResponseEntity<>(flightDTO, HttpStatus.OK);
     }
     @GetMapping("/{flightId}")
     public ResponseEntity<? super FlightDTO> getFlightById(@PathVariable Long flightId){
@@ -30,9 +35,21 @@ public class FlightController {
             FlightDTO savedFlight = flightService.createNewFlight(flightDTO);
             return new ResponseEntity<>(savedFlight,HttpStatus.CREATED);
     }
+    @PostMapping("/flightCrew")
+    public ResponseEntity<?> createNewFlightWithCrew(@Valid @RequestBody FlightDTO flightDTO){
+        FlightDTO savedFlight = flightService.createNewFlightWithCrew(flightDTO);
+        return new ResponseEntity<>(savedFlight,HttpStatus.CREATED);
+    }
+    @PostMapping("/flightCrew/assign/{flightId}")
+    public ResponseEntity<?> assignCrewMemberToFlight(@PathVariable Long flightId, @RequestBody List<Long> userIds){
+        FlightDTO savedFlight = flightService.assignFlightCrew(flightId,userIds);
+        return new ResponseEntity<>(savedFlight,HttpStatus.CREATED);
+    }
     @PatchMapping("/{flightId}")
     public ResponseEntity<? super FlightDTO> updateFlight(@PathVariable Long flightId,@Valid @RequestBody FlightDTO flightDTO){
         FlightDTO updatedFlight= flightService.updateExistsFlight(flightId,flightDTO);
         return new ResponseEntity<>(updatedFlight,HttpStatus.OK);
     }
+
+
 }
