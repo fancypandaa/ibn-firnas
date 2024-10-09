9374b3a0f3c1931213bb946442f584f06aa26b3a
liosub <mlio64565@gmail.com>
2024-10-09 20:24:40 +0100

    add find in array method

--- src/main/java/com/ibn/firnas/repostiories/UserDetailsRepository.java
+++ src/main/java/com/ibn/firnas/repostiories/UserDetailsRepository.java
@@ -3,7 +3,8 @@ package com.ibn.firnas.repostiories;
 import com.ibn.firnas.domain.UserDetails;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
+import java.util.*;
 @Repository
 public interface UserDetailsRepository extends JpaRepository<UserDetails,Long> {
+    Set<UserDetails> findByUserIdIn(List<Long> userIds);
 }
