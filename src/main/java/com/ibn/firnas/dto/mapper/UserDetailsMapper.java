429562efc10579daa6bd4349e505e816ce8c65e6
liosub <mlio64565@gmail.com>
2024-10-13 00:39:23 +0100

    add method for struct userDetails object map

--- src/main/java/com/ibn/firnas/dto/mapper/UserDetailsMapper.java
+++ src/main/java/com/ibn/firnas/dto/mapper/UserDetailsMapper.java
@@ -1,13 +1,27 @@
 package com.ibn.firnas.dto.mapper;
 
+import com.ibn.firnas.domain.Location;
 import com.ibn.firnas.domain.UserDetails;
 import com.ibn.firnas.dto.airCrew.UserDetailsDTO;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 import org.mapstruct.factory.Mappers;
-
+import java.util.*;
 @Mapper(componentModel = "spring")
 public interface UserDetailsMapper {
+    @Named(value = "extractCurrentLocationId")
+    default Long getCurrentLocation(Set<Location> locations){
+        for(Location location:locations){
+            if(location.getIsActive()){
+                return location.getLocationId();
+            }
+        }
+        return locations.stream().iterator().next().getLocationId();
+    }
     UserDetailsMapper INSTANCE = Mappers.getMapper(UserDetailsMapper.class);
+    @Mapping(source = "salary.salaryId", target = "salaryId")
+    @Mapping(source = "locations" ,target = "locationId",qualifiedByName = "extractCurrentLocationId")
     UserDetailsDTO userDetailsToUserDetailsDTO(UserDetails userDetails);
     UserDetails userDetailsDTOtoUserDetails(UserDetailsDTO userDetailsDTO);
 }
