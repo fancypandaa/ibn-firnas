e591a26f62aee4587cbddf809142d6a26aa98a9c
liosub <mlio64565@gmail.com>
2024-11-17 23:07:19 +0000

    change methods access modifiers

--- src/main/java/com/ibn/firnas/specifications/UserDetailsSpecification.java
+++ src/main/java/com/ibn/firnas/specifications/UserDetailsSpecification.java
@@ -3,12 +3,11 @@ import com.ibn.firnas.domain.UserDetails;
 import com.ibn.firnas.domain.UserDetails_;
 import org.springframework.data.jpa.domain.Specification;
 
-import java.util.Optional;
 public class UserDetailsSpecification {
-        private static Specification<UserDetails> nameLike(Optional<String> firstName, Optional<String> lastName){
+        public static Specification<UserDetails> nameLike(String firstName,String lastName){
             return (root,query,criteriaBuilder) -> criteriaBuilder.or(
-                    criteriaBuilder.like(root.get(UserDetails_.FIRST_NAME),"%"+firstName.get()+"%"),
-                    criteriaBuilder.like(root.get(UserDetails_.LAST_NAME),"%"+lastName.get()+"%")
+                    criteriaBuilder.like(root.get(UserDetails_.FIRST_NAME),"%"+firstName+"%"),
+                    criteriaBuilder.like(root.get(UserDetails_.LAST_NAME),"%"+lastName+"%")
                     );
         }
 
