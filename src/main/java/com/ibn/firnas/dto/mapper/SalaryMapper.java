1f72f21f7fc49f4bd14582cbdcc08a18cbc4a9c3
liosub <mlio64565@gmail.com>
2024-11-17 22:43:38 +0000

    add salary list dto mapper

--- src/main/java/com/ibn/firnas/dto/mapper/SalaryMapper.java
+++ src/main/java/com/ibn/firnas/dto/mapper/SalaryMapper.java
@@ -4,10 +4,11 @@ import com.ibn.firnas.domain.Salary;
 import com.ibn.firnas.dto.airCrew.SalaryDTO;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
-
+import java.util.List;
 @Mapper(componentModel = "spring")
 public interface SalaryMapper {
     SalaryMapper INSTANCE = Mappers.getMapper(SalaryMapper.class);
     SalaryDTO salarytoSalaryDTO(Salary salary);
     Salary salaryDTOtoSalary(SalaryDTO salaryDTO);
+    List<SalaryDTO> salariesToSalaryDTOs(List<Salary> salaries);
 }
