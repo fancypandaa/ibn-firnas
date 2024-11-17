dcd50a126f72bb10554c83c854899b22a5499349
liosub <mlio64565@gmail.com>
2024-11-17 17:22:55 +0000

    remove unused import library

--- src/main/java/com/ibn/firnas/domain/Location.java
+++ src/main/java/com/ibn/firnas/domain/Location.java
@@ -1,7 +1,6 @@
 package com.ibn.firnas.domain;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
