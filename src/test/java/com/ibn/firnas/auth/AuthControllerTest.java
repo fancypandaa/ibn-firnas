87d308abe86ff75634a8fc81d7ec8cb6805b70c0
fancypanda <nlio7585@proton.me>
2024-12-13 19:17:06 +0000

    add auth test

---
+++ src/test/java/com/ibn/firnas/auth/AuthControllerTest.java
@@ -0,0 +1,17 @@
+package com.ibn.firnas.auth;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AuthControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void login() {
+    }
+}
